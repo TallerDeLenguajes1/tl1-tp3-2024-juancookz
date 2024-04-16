d45a3757a2efd9faea7c70eaa8567419aaacc833
“juancookz” <jcavila383@gmail.com>
2024-04-16 13:06:02 -0300

    funcion mostrar matriz completada

--- tp3_2.c
+++ tp3_2.c
@@ -4,18 +4,12 @@
 #define FILAS 5
 #define COLS 12
 void cargarMatriz(int *matriz, int filas, int columnas);
+void mostrarMatriz(int matriz[FILAS][COLS]);
 int main()
 {
     int matriz[FILAS][COLS];
     cargarMatriz(&(matriz[0][0]), FILAS, COLS);
-    for (int i = 0; i < FILAS; i++)
-    {
-        for (int j = 0; j < COLS; j++)
-        {
-            printf("%d|", matriz[i][j]);
-        }
-        printf("\n");
-    }
+    mostrarMatriz(matriz);
     return 0;
 }
 void cargarMatriz(int *matriz, int filas, int columnas)
@@ -30,5 +24,16 @@ void cargarMatriz(int *matriz, int filas, int columnas)
             dir = (matriz + i * columnas + j);
             *dir = n;
         }
+    }
+}
+void mostrarMatriz(int matriz[FILAS][COLS])
+{
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%d|", matriz[i][j]);
+        }
+        printf("\n");
     }
 }
