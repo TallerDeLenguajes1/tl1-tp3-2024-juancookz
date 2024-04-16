882a084b0e66b698c4748e841f8b255aba2e6cee
“juancookz” <jcavila383@gmail.com>
2024-04-16 12:53:40 -0300

    funcion de carga de matriz completada

---
+++ tp3_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define FILAS 5
+#define COLS 12
+void cargarMatriz(int *matriz, int filas, int columnas);
+int main()
+{
+    int matriz[FILAS][COLS];
+    cargarMatriz(&(matriz[0][0]), FILAS, COLS);
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%d|", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+void cargarMatriz(int *matriz, int filas, int columnas)
+{
+    srand(time(NULL));
+    int n, *dir;
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            n = (rand() % 40000) + 10000;
+            dir = (matriz + i * columnas + j);
+            *dir = n;
+        }
+    }
+}
