b1619d79d4cb169289fe69422604742352e72139
juancookz <jcavila383@gmail.com>
2024-04-22 17:05:19 -0300

    Funcion maximos, minimos y numeroAMes completada

--- tp3_2.c
+++ tp3_2.c
@@ -6,12 +6,15 @@
 void cargarMatriz(int *matriz, int filas, int columnas);
 void mostrarMatriz(int matriz[FILAS][COLS]);
 void promedioAnual(int matriz[FILAS][COLS]);
+void valorMinMax(int matriz[FILAS][COLS]);
+char *numeroAMes(int num);
 int main()
 {
     int matriz[FILAS][COLS];
     cargarMatriz(&(matriz[0][0]), FILAS, COLS);
     mostrarMatriz(matriz);
     promedioAnual(matriz);
+    valorMinMax(matriz);
     return 0;
 }
 void cargarMatriz(int *matriz, int filas, int columnas)
@@ -22,7 +25,7 @@ void cargarMatriz(int *matriz, int filas, int columnas)
     {
         for (int j = 0; j < columnas; j++)
         {
-            n = (rand() % 40000) + 10000;
+            n = (rand() % 40001) + 10000;
             dir = (matriz + i * columnas + j);
             *dir = n;
         }
@@ -39,7 +42,8 @@ void mostrarMatriz(int matriz[FILAS][COLS])
         printf("\n");
     }
 }
-void promedioAnual(int matriz[FILAS][COLS]){
+void promedioAnual(int matriz[FILAS][COLS])
+{
     int anio = 2025;
     float promedio;
     printf("\nPromedio de ganancias por anio:\n");
@@ -48,12 +52,76 @@ void promedioAnual(int matriz[FILAS][COLS]){
         printf("%d: ", anio);
         for (int j = 0; j < COLS; j++)
         {
-             promedio += matriz[i][j];
+            promedio += matriz[i][j];
         }
-        promedio = promedio/COLS;
+        promedio = promedio / COLS;
         printf("%.1f", promedio);
         anio++;
         promedio = 0;
         printf("\n");
+    }
+}
+void valorMinMax(int matriz[FILAS][COLS])
+{
+    int num, mayor, menor = 50000, mesMayor, anioMayor, mesMenor, anioMenor;
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            num = matriz[i][j];
+            if (num > mayor)
+            {
+                mayor = num;
+                mesMayor = j + 1;
+                anioMayor = i + 2025;
+            }
+            if (num < menor)
+            {
+                menor = num;
+                mesMenor = j + 1;
+                anioMenor = i + 2025;
+            }
+        }
+    }
+    char *nombreMes;
+
+    nombreMes = numeroAMes(mesMenor);
+    printf("\nMenor: %d\n %s/%d", menor, nombreMes, anioMenor);
+
+    nombreMes = numeroAMes(mesMayor);
+    printf("\nMayor: %d\n %s/%d", mayor, nombreMes, anioMayor);
+}
+char *numeroAMes(int num)
+{
+    switch (num)
+    {
+    case 1:
+        return "Enero";
+    case 2:
+        return "Febrero";
+    case 3:
+        return "Marzo";
+    case 4:
+        return "Abril";
+    case 5:
+        return "Mayo";
+    case 6:
+        return "Junio";
+    case 7:
+        return "Julio";
+    case 8:
+        return "Agosto";
+    case 9:
+        return "Septiembre";
+    case 10:
+        return "Octubre";
+    case 11:
+        return "Noviembre";
+    case 12:
+        return "Diciembre";
+        break;
+    default:
+        return "Mes invalido";
+        break;
     }
 }
